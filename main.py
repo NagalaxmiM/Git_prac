b6f28730ada2dd53432cbb1250631c1b57f344a0
Nagalaxmi <nagalaxmi.m@goavega.com>
2020-12-12 23:56:42 +0530

    updated

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from sanic import Sanic, response
-
+import requests
 app = Sanic(__name__)
 
-@app.route("/external/GitHub/users", methods = ['GET','POST'])
-def on_post(request):
-    return response.redirect("https://api.github.com/users?per_page=10")
-
+@app.route("/external/GitHub/users")
+async def on_post(request):
+    my_list = []
+    d = dict()
+    data = requests.get('https://api.github.com/users?per_page=10').json()
+    for i in data:
+        d = dict()
+        for key,value in i.items():
+            if key in ['id','login','url']:
+                d.update({key:value})
+        my_list.append(d)
+        del d
+    print(my_list)
+    return response.json(my_list)    
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(host = '192.168.43.9',debug=True)
