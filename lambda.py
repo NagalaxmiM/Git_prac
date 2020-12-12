1b26350c447624697ee3785bbc6accd594734af5
Nagalaxmi <nagalaxmi.m@goavega.com>
2020-12-12 13:33:07 +0530

    deleted

--- lambda.py
+++
@@ -1,9 +0,0 @@
-person = [{'name': 'Harry','house': 'Gryffindor'},{'name':'Cho', 'house': 'Ravenclaw'}, {'name': 'Dreco','house': 'Slytherin'}]
-
-
-
-person.sort(key= lambda person: person['name'])
-
-
-
-print(person)
--- prac.py
+++
@@ -1,2 +0,0 @@
-nums = [str(n) for n in range(20)]
-print("".join(nums))
