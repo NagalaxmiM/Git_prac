b538e4dd68c8cbb5d3bbab3a238b754d115d1d12
Nagalaxmi <nagalaxmi.m@goavega.com>
2020-12-12 14:37:22 +0530

    API

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from sanic import Sanic, response
 
 app = Sanic(__name__)
 
-#app.blue_print(my_bp)
-
 @app.route("/external/GitHub/users", methods = ['GET','POST'])
 def on_post(request):
     return response.redirect("https://api.github.com/users?per_page=10")
