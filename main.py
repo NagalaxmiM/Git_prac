949c2ecf3d0b0109f30d2afc4da59a799eac58aa
Nagalaxmi <nagalaxmi.m@goavega.com>
2020-12-12 14:34:51 +0530

    'dnone'

---
+++ main.py
@@ -0,0 +1,13 @@
+from sanic import Sanic, response
+
+app = Sanic(__name__)
+
+#app.blue_print(my_bp)
+
+@app.route("/external/GitHub/users", methods = ['GET','POST'])
+def on_post(request):
+    return response.redirect("https://api.github.com/users?per_page=10")
+
+if __name__ == "__main__":
+    app.run(host ="192.168.43.9", port = 8080,debug=True)
+    
