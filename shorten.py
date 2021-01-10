84661ddfabdc09709cfb442b3d3ef64e9a721fd1
Nagalaxmi <nagalaxmi.m@goavega.com>
2021-01-10 20:55:48 +0530

    done

--- shorten.py
+++ shorten.py
@@ -1,26 +1,31 @@
 from sanic import Blueprint, response
 from models import Shorten_url
 from crud import Session
+import json 
+import ast
 
 bp = Blueprint('shortenUrlBlueprint')
 
 @bp.route("/POST/shorten", methods=['POST','GET'])
 async def on_post(request):
     s = Session()
-    
-    if s.query(Shorten_url).filter_by(url=request.json['url']).first().url:
-        short_url = s.query(Shorten_url).filter_by(url=request.json['url']).first().short_url
-        id = s.query(Shorten_url).filter_by(url=request.json['url']).first().id
+    url_bytes = request.body
+    url_decoded = url_bytes.decode("UTF-8")
+    url = ast.literal_eval(url_decoded)
+    #url = json.loads(url)
+    if s.query(Shorten_url).filter_by(url=url['url']).first().url:
+        short_url = s.query(Shorten_url).filter_by(url=url['url']).first().short_url
+        id = s.query(Shorten_url).filter_by(url=url['url']).first().id
         s.close()
-        return response.json({"url": request.json['url'], "shortUrl": short_url, "id": id, "success": "true"})
+        return response.json({"url": url['url'], "shortUrl": short_url, "id": id, "success": "true"})
     
-    link = Shorten_url(url=request.json['url'])
+    link = Shorten_url(url=url['url'])
     s.add(link)
     s.commit()
-    short_url = s.query(Shorten_url).filter_by(url=request.json['url']).first().short_url
-    id = s.query(Shorten_url).filter_by(url = request.json['url']).first().id
+    short_url = s.query(Shorten_url).filter_by(url=url['url']).first().short_url
+    id = s.query(Shorten_url).filter_by(url = url['url']).first().id
     s.close()
-    return response.json({"url": request.json['url'], "shortUrl": short_url, "id": id, "success": "true"})
+    return response.json({"url": url['url'], "shortUrl": short_url, "id": id, "success": "true"})
 
 @bp.route("/DELETE/shorten/<id>")
 async def on_delete(request,id):
