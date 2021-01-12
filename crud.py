d7d5bb0cca9057fa13ed0fef4262df01b9eeff12
Nagalaxmi <nagalaxmi.m@goavega.com>
2021-01-12 13:27:57 +0530

    done

--- crud.py
+++ crud.py
@@ -1,5 +1,4 @@
 from models import Base
-from contextlib import contextmanager
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from config import DATABASE_URI
@@ -8,17 +7,6 @@ engine = create_engine(DATABASE_URI)
 
 Session = sessionmaker(bind = engine)
 
-@contextmanager
-def session_scope():
-    session = Session()
-    try:
-        yield session
-        session.commit
-    except Exception:
-        session.rollback()
-        raise
-    finally:
-        session.close()
-        
+
 Base.metadata.create_all(engine)
 
--- shorten.py
+++ shorten.py
@@ -2,30 +2,33 @@ from sanic import Blueprint, response
 from models import Shorten_url
 from crud import Session
 import json 
-import ast
-
 bp = Blueprint('shortenUrlBlueprint')
 
-@bp.route("/POST/shorten", methods=['POST'])
-async def on_post(request):
+@bp.route("/generate_url/<url_dict>", methods=['POST','GET'])  
+async def gen_url(request, url_dict): 
     s = Session()
-    url_bytes = request.body
-    url_decoded = url_bytes.decode("UTF-8")
-    url = ast.literal_eval(url_decoded)
-    #url = json.loads(url)
-    if s.query(Shorten_url).filter_by(url=url['url']).first().url:
-        short_url = s.query(Shorten_url).filter_by(url=url['url']).first().short_url
-        id = s.query(Shorten_url).filter_by(url=url['url']).first().id
-        s.close()
-        return response.json({"url": url['url'], "shortUrl": short_url, "id": id, "success": "true"})
     
-    link = Shorten_url(url=url['url'])
+    if s.query(Shorten_url).filter_by(url=url_dict).first().url:
+        short_url = s.query(Shorten_url).filter_by(url=url_dict).first().short_url
+        id = s.query(Shorten_url).filter_by(url=url_dict).first().id
+        s.close()
+        return response.json({"url": url_dict, "shortUrl": short_url, "id": id, "success": "true"})
+    
+    link = Shorten_url(url=url_dict)
     s.add(link)
     s.commit()
-    short_url = s.query(Shorten_url).filter_by(url=url['url']).first().short_url
-    id = s.query(Shorten_url).filter_by(url = url['url']).first().id
+    short_url = s.query(Shorten_url).filter_by(url=url_dict).first().short_url
+    id = s.query(Shorten_url).filter_by(url = url_dict).first().id
     s.close()
-    return response.json({"url": url['url'], "shortUrl": short_url, "id": id, "success": "true"})
+    return response.json({"url": url_dict, "shortUrl": short_url, "id": id, "success": "true"})
+
+
+@bp.route("/POST/shorten", methods=['POST','GET'])
+async def on_post(request):
+    data = request.body
+    url_string = data.decode('utf-8').replace("'", '"')
+    url_dict = json.loads(url_string)
+    return response.redirect('/generate_url/<url_dict>',url_dict['url'] )
 
 @bp.route("/DELETE/shorten/<id>")
 async def on_delete(request,id):
