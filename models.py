3889121bb0166d63600f53609047b0084c5efd1c
Nagalaxmi <nagalaxmi.m@goavega.com>
2021-01-13 10:30:32 +0530

    done

--- models.py
+++ models.py
@@ -11,17 +11,19 @@ class Shorten_url(Base):
     url = Column(String(1024))
     short_url = Column(String(1024), unique = True)
     
-    def __init__(self, **kwargs):
+    self.url=url
+    def assign(self, **kwargs):
         super().__init__(**kwargs)
         self.short_url = self.generate_short_url(self.url)
 
     def generate_short_url(self, url):
         
         short_url = hashlib.sha1(self.url.encode()).hexdigest()
-        link = self.query.filter_by(short_url=short_url).first()
-        
-        if link:
-            return self.generate_short_url(self, url)
         return short_url
+        #link = self.query.filter_by(short_url=short_url).first()
+        
+        #if link:
+        #    return self.generate_short_url(self, url)
+        #return short_url
     
     
--- shorten.py
+++ shorten.py
@@ -10,7 +10,22 @@ async def on_post(request):
     url_dict = request.json
     
     #checking if the entered url is already in the database, if yes return already created short_url
+    if s.query(Shorten_url).filter_by(url=url_dict['url']).first() == None:
+        link = Shorten_url(url=url_dict['url'])
+        s.add(link)
+        s.commit()
+        short_url = s.query(Shorten_url).filter_by(url=url_dict['url']).short_url
+        id = s.query(Shorten_url).filter_by(url = url_dict['url']).id
+        s.close()
+        return response.json({"url": url_dict['url'], "shortUrl": short_url, "id": id, "success": "true"})
 
+    else:
+        short_url = s.query(Shorten_url).filter_by(url=url_dict['url']).first().short_url
+        id = s.query(Shorten_url).filter_by(url=url_dict['url']).first().id
+        s.close()
+        return response.json({"url": url_dict['url'], "shortUrl": short_url, "id": id, "success": "true"})
+
+    '''
     if s.query(Shorten_url).filter_by(url=url_dict['url']).first().url:
         short_url = s.query(Shorten_url).filter_by(url=url_dict['url']).short_url
         id = s.query(Shorten_url).filter_by(url=url_dict['url']).id
@@ -24,7 +39,7 @@ async def on_post(request):
         id = s.query(Shorten_url).filter_by(url = url_dict['url']).id
         s.close()
         return response.json({"url": url_dict['url'], "shortUrl": short_url, "id": id, "success": "true"})
-    
+    '''
 
 @bp.route("/DELETE/shorten/<id>")
 async def on_delete(request,id):
