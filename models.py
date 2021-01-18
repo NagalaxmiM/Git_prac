6748a903f82881ed3f019bb07b457bf391e6774d
Nagalaxmi <nagalaxmi.m@goavega.com>
2021-01-18 11:35:47 +0530

    done

--- models.py
+++ models.py
@@ -22,26 +22,27 @@ class Shorten_url(Base):
     short_url = Column(String(1024), unique = True)
         
     def __init__(self, **kwargs):
-        self.url = url
+        super().__init__(**kwargs)
+        
         self.short_url = self.generate_short_url(self.url)
        
     def generate_short_url(self, url):
         s = Session()      
         url = self.url
-        short_url = hashlib.sha1(url.encode()).hexdigest()[0:7]
+        short_url = "https://"+hashlib.sha1(url.encode()).hexdigest()[0:7]
         link = s.query(Shorten_url).filter_by(short_url=short_url).first()
         
         while link!= None:
             characters = string.digits + string.ascii_letters
             adders = ''.join(choices(characters, k=3))
             url = self.url.encode()+adders.encode()
-            short_url = hashlib.sha1(url).hexdigest()[0:7]
+            short_url = "https://"+hashlib.sha1(url).hexdigest()[0:7]
             link = s.query(Shorten_url).filter_by(short_url=short_url).first()
             if link_!= None:
                 url_list = list(url)
                 random.shuffle(url_list)
                 shuffled_url = ''.join(url_list)
-                short_url = hashlib.sha1(shuffled_url.encode()).hexdigest()[0:7]
+                short_url = "https://"+hashlib.sha1(shuffled_url.encode()).hexdigest()[0:7]
                 link = s.query(Shorten_url).filter_by(short_url=short_url).first()
         s.close()
         return short_url
